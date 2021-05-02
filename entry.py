f338e19854e7c311a0f332d00dca0b6e0e322ec7
malloryeastburn <mallorykaitlin@gmail.com>
2021-05-02 15:22:09 -0500

    Video 4:  Entry Widgets

---
+++ entry.py
@@ -0,0 +1,34 @@
+#Purpose: Creating button widgets
+
+#Import everything from tkinter
+from tkinter import *
+
+#needed
+root = Tk()
+
+#creating entry widget (input box)
+e = Entry(root)
+#optional parameters
+#width=50
+#bg="blue"
+#fg="white"
+#borderwidth=5
+e.pack()
+e.insert(0, "Enter Your Name...")
+#create a funciton for the button click
+def myClick():
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
+    myLabel.pack()
+
+
+
+
+
+#define the thing
+myButton = Button(root, text = "Enter Your Name", command=myClick) 
+
+#display the thing
+myButton.pack()
+
+root.mainloop()
